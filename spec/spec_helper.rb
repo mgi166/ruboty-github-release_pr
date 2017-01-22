62e97585a3099f51fe1eaffdce598a53bb15c7b6
mgi166 <skskoari@gmail.com>
2017-01-22 10:41:39 +0900

    Require source codes for test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,8 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require "./lib/ruboty/github/release_pr"
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
